b95b7b594f2c53a3205481086a799530b2caa85e
Madison Cagle <78162094+mac3141@users.noreply.github.com>
2021-09-29 15:17:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,21 +7,21 @@ const container = document.getElementById("container");
 
 let activeIndex = 217;
 
-for(let i=0; i < SQUARES; i++) {
-    const square = document.createElement('div');
+for(let i = 0; i < SQUARES; i++) {
+    const square = document.createElement("div");
     square.classList.add("square");
 
     // on mouseover, add random color
-    square.addEventListener( 'mouseover', () => setColor(square));
+    square.addEventListener("mouseover", () => setColor(square));
 
     // on mouseout, remove color
-    square.addEventListener('mouseout', ()=> removeColor(square));
-    
+    square.addEventListener("mouseout", () => removeColor(square));
+
     container.appendChild(square);
 }
 
 function getRandomColor() {
-    return colors[ Math.floor( Math.random() * colors.length) ];
+    return colors[Math.floor(Math.random() * colors.length)];
 }
 
 function setColor(element) {
@@ -32,23 +32,23 @@ function setColor(element) {
 
 function removeColor(element) {
     element.style.background = "#1d1d1d";
-    element.style.boxShadow = `0 0 2px #1d1d1d, 0 0 10px #1d1d1d`;
+    element.style.boxShadow = "0 0 2px #1d1d1d";
 }
 
 function clear() {
     const squares = document.getElementsByClassName("square");
-
-    for( let square of squares) {
-        removeColor( square);
+    
+    for(let square of squares) {
+        removeColor(square);
     }
 }
 
 function blink() {
     const squares = document.getElementsByClassName("square");
-
-    for( let square of squares) {
-        setTimeout( ()=> setColor(square), 100);
-        setTimeout( ()=> removeColor(square), 2000);
+    
+    for(let square of squares) {
+        setColor(square);
+        setTimeout(() => removeColor(square), 750);
     }
 }
 
@@ -58,26 +58,23 @@ function getSquareByIndex(index) {
     return squares[index];
 }
 
-document.addEventListener('keypress', function( event ) {
-
-    if( event.code === "Space" ) {
+document.addEventListener("keypress", function(event) {
+    if(event.code === "Space") {
         console.log("SPACE");
         clear();
     }
 
-    if( event.code === "Enter") {
+    if(event.code === "Enter") {
         blink();
     }
 
-
-    if( event.key == 'a') {
-        if ( activeIndex % WIDTH === 0) {
-            activeIndex += WIDTH;    
+    if(event.key == 'a') {
+        if(activeIndex % WIDTH === 0) {
+            activeIndex += WIDTH;
         }
+
         activeIndex = activeIndex - 1;
-        
-        setColor(  getSquareByIndex(activeIndex) );
+
+        setColor(getSquareByIndex(activeIndex));
     }
-
-
 });
